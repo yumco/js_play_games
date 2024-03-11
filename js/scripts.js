180165df80ff411e70e4f6e95b827d6c23e517c2
yumco <pmwoj.022@gmail.com>
2024-03-11 13:10:17 +0900

    24/03/11

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-const userInput =prompt('가위 바위 보!');
 
-if (userInput !=='가위'&& userInput!=='바위'&&userInput!=='보'){
+const SCISSORS = '가위';
+const ROCK = '바위';
+const PAPER = '보';
+
+const userInput = prompt('가위, 바위, 보');
+
+if (userInput !==SCISSORS&& userInput!==ROCK &&userInput!==PAPER){
     alert('가위, 바위, 보 중 하나를 입력하세요');
     } else {
     let comInput;
@@ -9,32 +14,48 @@ if (userInput !=='가위'&& userInput!=='바위'&&userInput!=='보'){
     const rnd = Math.random();
     
     if(rnd < 0.33){
-        comInput = '가위';
+        comInput = SCISSORS;
     
     } else if(rnd < 0.66) {
-        comInput ='바위';
+        comInput = ROCK;
     } else {
-        comInput = '보'
+        comInput = PAPER;
     }
     
-    if(userInput === '가위'){
-    //컴퓨터 패 비교
-    if(comInput === '가위'){
-        alert('컴퓨터: ' +comInput + ' -컴퓨터와 비겼습니다');
-    }
-    } else if (comInput === '바위') {
-    //컴퓨터 패 비교
-    alert('컴퓨터: ' + comInput+ ' -컴퓨터에게 졌습니다');
+    if(userInput === SCISSORS){
+        if(comInput === SCISSORS){
+         alert('컴퓨터: ' + comInput + ' -컴퓨터와 비겼습니다.');
+    }else if (comInput === ROCK) {
+        alert('컴퓨터: ' + comInput+ ' -컴퓨터에게 졌습니다.');
     
      } else {
-    // 컴퓨터 패 비교
-    alert('컴퓨터: ' + comInput + ' -컴퓨터를 이겼습니다');
-    }
-   
-
+        alert('컴퓨터: ' + comInput + ' -컴퓨터를 이겼습니다.');
+        }    
+    } 
 }
 
-
-
-
-
+if(userInput === SCISSORS) {
+    if(comInput === SCISSORS) {
+        alert('컴퓨터: ' + comInput + '- 컴퓨터와 비겼습니다.' );
+    } else if (comInput  === ROCK){
+        alert('컴퓨터: ' +comInput + '- 컴퓨터에게 졌습니다.');
+    } else {
+        alert('컴퓨터 : ' + comInput + '- 컴퓨터를 이겼습니다');
+    } else if (userInput === ROCK){
+       if(comInput === SCISSORS){
+        alert('컴퓨터 :' +comInput + '- 컴퓨터를 이겼습니다');
+    } else if (comInput===ROCK){
+        alert('컴퓨터 :' + comInput +'- 컴퓨터와 비겼습니다');
+    }else {
+        alert('컴퓨터: ' +comInput + '- 컴퓨터에게 졌습니다');
+    } else {
+        if (comInput === SCISSORS){
+            alert('컴퓨터: ' +comInput + '- 컴퓨터에게 졌습니다')
+        } else if (comInput === ROCK){
+            alert('컴퓨터 : ' + comInput + '- 컴퓨터에게 이겼습니다.')
+        } else {
+            alert('컴퓨터: ' + comInput + '- 컴퓨터와 비겼습니다')
+        }
+    }
+}
+}
